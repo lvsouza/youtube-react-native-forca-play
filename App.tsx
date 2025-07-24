1810b93b353a4a2f658285885bd8783ffc9f8cf9
Lucas Souza <ls18017@gmail.com>
2025-07-24 15:02:52 -0300

    feat: add local fonts with expo fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import { Text, View } from 'react-native';
 
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded] = useFonts({
+    PoppinsRegular: require('./assets/fonts/Poppins/Poppins-Regular.ttf'),
+    PoppinsBold: require('./assets/fonts/Poppins/Poppins-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+
+  if (!loaded) return null;
+
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontFamily: 'PoppinsBold' }}>Hello world</Text>
     </View>
   );
 }
