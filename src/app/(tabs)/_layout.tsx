b071fc22dc10a0489e52f4a438a5ec7dcc9ed777
Lucas Souza <ls18017@gmail.com>
2025-07-26 15:48:31 -0300

    feat: add styles for tab

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,20 +1,75 @@
-import { Tabs } from 'expo-router';
+import { useState } from 'react';
+import { Tabs, TabSlot, TabList, TabTrigger, TabTriggerSlotProps } from 'expo-router/ui';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Pressable, Text, View } from 'react-native';
+
 import { theme } from '../../shared/themes/Theme';
 
 
 export default function Layout() {
+  const insets = useSafeAreaInsets();
 
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: theme.colors.paper,
-        },
-        sceneStyle: {
-          backgroundColor: theme.colors.background,
-        },
+    <View
+      style={{
+        flex: 1,
+        marginTop: insets.top,
+        marginBottom: insets.bottom,
+        marginLeft: insets.left,
+        marginRight: insets.right,
       }}
-    />
+    >
+      <Tabs>
+        <TabList
+          style={{
+            gap: 16,
+            paddingVertical: 8,
+            paddingHorizontal: 16,
+            marginHorizontal: 'auto',
+            borderRadius: theme.corner.large,
+            backgroundColor: theme.colors.paper,
+          }}
+        >
+          <TabTrigger name="index" href="/" asChild>
+            <CustomTabButton
+              text='Início'
+            />
+          </TabTrigger>
+          <TabTrigger name="About" href="/About" asChild>
+            <CustomTabButton
+              text='Sobre'
+            />
+          </TabTrigger>
+        </TabList>
+
+
+        <TabSlot />
+      </Tabs>
+    </View >
   );
 }
+
+type TCustomTabButtonProps = TabTriggerSlotProps & {
+  text: string;
+}
+const CustomTabButton = ({ isFocused, text, onPressIn, onPressOut, ...props }: TCustomTabButtonProps) => {
+  const [pressed, setPressed] = useState(false);
+
+  return (
+    <Pressable
+      {...props}
+      className='px-2 py-1'
+      style={{ opacity: pressed ? 0.5 : 1 }}
+      onPressIn={(event) => { setPressed(true); onPressIn?.(event); }}
+      onPressOut={(event) => { setPressed(false); onPressOut?.(event); }}
+    >
+      <Text
+        className='text-text text-base font-regular'
+        style={{ textDecorationLine: isFocused ? 'underline' : undefined }}
+      >
+        {text}
+      </Text>
+    </Pressable>
+  );
+}
+
