7da7bed3e0f704ff54f3724302300ed4384f41ec
Lucas Souza <ls18017@gmail.com>
2025-07-30 16:03:29 -0300

    feat: add tip and maskedWord in MatchOngoing

--- src/app/matches/MatchOngoing.tsx
+++ src/app/matches/MatchOngoing.tsx
@@ -2,11 +2,30 @@ import { ScrollView, Text, View } from 'react-native';
 
 
 export default function MatchOngoing() {
+  const maskedWord = ['J', 'O', '_', 'O'];
+  const tip = 'Algo para se divertir';
+
 
   return (
-    <ScrollView className='flex-1 p-2'>
-      <View className='gap-2'>
-        <Text>MatchOngoing</Text>
+    <ScrollView className='flex-1 p-2 pt-6'>
+      <View className='gap-6 items-center'>
+
+
+        <Text className='text-text font-regular text-xl underline'>
+          {tip}
+        </Text>
+
+        <View className='flex-row flex-wrap gap-2 items-center justify-center'>
+          {maskedWord.map((letter, index) => (
+            <View key={index} className='border-b-4 border-text h-9 w-9'>
+              <Text className='text-text font-bold text-2xl text-center'>
+                {letter === '_' ? '' : letter}
+              </Text>
+            </View>
+          ))}
+        </View>
+
+
       </View>
     </ScrollView>
   );
