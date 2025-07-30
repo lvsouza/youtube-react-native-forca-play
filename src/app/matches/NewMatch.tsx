078223dcd51681a5c0e820286d11500e972d2f2c
Lucas Souza <ls18017@gmail.com>
2025-07-30 14:42:38 -0300

    feat: add custom component of select

--- src/app/matches/NewMatch.tsx
+++ src/app/matches/NewMatch.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import { ScrollView, Text, View } from 'react-native';
+
+import { Select } from '../../shared/components/new-match/Select';
 
 
 export default function NewMatch() {
 
+  const [selected, setSelected] = useState('');
+
+
   return (
     <ScrollView className='flex-1 p-2'>
       <View className='gap-2'>
-        <Text>New match</Text>
+        <Text>New match {selected}</Text>
+
+        <Select
+          value={selected}
+          label='Dificuldade'
+          onGetItemLabel={(item) => item?.name || ''}
+          onSelect={(selectedItem) => setSelected(selectedItem.id)}
+          onGetSelected={(selectedValue, item) => item.id === selectedValue}
+          data={[
+            { id: '1', name: 'Um' },
+            { id: '2', name: 'Dois' },
+            { id: '3', name: 'Três' },
+            { id: '4', name: 'Quatro' },
+          ]}
+        />
       </View>
     </ScrollView>
   );
---
+++ src/shared/components/new-match/Select.tsx
@@ -0,0 +1,64 @@
+import { Text, View } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import SelectDropdown from 'react-native-select-dropdown'
+
+import { theme } from '../../themes/Theme';
+
+
+type TSelectProps = {
+  data: any[];
+  label: string;
+  value: string | number | null;
+  onSelect: (item: any) => void;
+  onGetItemLabel: (item: any | undefined) => string | undefined;
+  onGetSelected: (value: string | number | null, item: any) => any;
+}
+export const Select = ({ data, label, onSelect, value, onGetSelected, onGetItemLabel }: TSelectProps) => {
+  return (
+    <View className='gap-1'>
+      <Text className='font-bold text-text text-sm'>
+        {label}
+      </Text>
+
+      <SelectDropdown
+        data={data}
+        dropdownOverlayColor='transparent'
+        onSelect={(selectedItem) => onSelect(selectedItem)}
+        defaultValue={data.find(item => onGetSelected(value, item))}
+        dropdownStyle={{
+          padding: 8,
+          marginTop: -24,
+          borderRadius: theme.corner.small,
+          backgroundColor: theme.colors.paper,
+        }}
+        renderButton={(selectedItem) => (
+          <View className='border-4 border-text rounded px-4 py-4 flex-row justify-between items-center'>
+            <Text className='text-text text-base font-regular'>
+              {onGetItemLabel(selectedItem) || 'Selecione'}
+            </Text>
+
+            <MaterialIcons
+              size={24}
+              name='arrow-drop-down'
+              color={theme.colors.text}
+            />
+          </View>
+        )}
+        renderItem={(renderedItem) => (
+          <View
+            className='p-4 rounded'
+            style={{
+              backgroundColor: onGetSelected(value, renderedItem)
+                ? theme.colors.background
+                : undefined
+            }}
+          >
+            <Text className='text-text font-regular text-base'>
+              {onGetItemLabel(renderedItem)}
+            </Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
